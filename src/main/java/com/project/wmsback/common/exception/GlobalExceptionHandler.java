8d450f196787783ce6ff75e5dca7fa8e9a2aef86
gh.jo <rjsgml1677@naver.com>
2026-07-19 16:14:05 +0900

    chore: 미처리 예외 서버 로깅 추가

--- src/main/java/com/project/wmsback/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/wmsback/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.project.wmsback.common.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException e) {
@@ -52,6 +56,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleException(Exception e) {
+        log.error("처리되지 않은 예외", e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Map.of("message", "서버 오류가 발생했습니다."));
