ab717911f5bdefad2dd76b40fe92fb978a2b76d1
Yang <chyeon2932@gmail.com>
2026-07-29 14:28:30 +0900

    feat: NbrSeqRepository 추가 (PESSIMISTIC_WRITE 락 + upsert)

---
+++ src/main/java/com/project/wmsback/master/repository/NbrSeqRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.project.wmsback.master.repository;
+
+import com.project.wmsback.master.entity.NbrSeq;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.LockModeType;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+import static com.project.wmsback.master.entity.QNbrSeq.nbrSeq;
+
+@RequiredArgsConstructor
+public class NbrSeqRepositoryImpl implements NbrSeqRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<NbrSeq> findByIdForUpdate(String ruleCd, String dyncKy) {
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(nbrSeq)
+                        .where(nbrSeq.ruleCd.eq(ruleCd), nbrSeq.dyncKy.eq(dyncKy))
+                        .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+                        .fetchOne());
+    }
+}
